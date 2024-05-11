58c1ce17d7792db83aada46db16e140b09411cf4
sametyorgunn <asametyorgun60@gmail.com>
2024-05-12 01:23:38 +0300

    Add project files.

---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://fxcjghbr:mtqXaN5pPGT7OTSMEF1KPij9boAM3fah@cougar.rmq.cloudamqp.com/fxcjghbr");
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false, autoDelete: false, durable: true);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false,consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+    //channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+   
+    channel.BasicNack(deliveryTag: e.DeliveryTag, multiple: false, requeue: true);
+
+}
+
+Console.Read();
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://fxcjghbr:mtqXaN5pPGT7OTSMEF1KPij9boAM3fah@cougar.rmq.cloudamqp.com/fxcjghbr");
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false,autoDelete:false,durable:true); //durable true mesajların kalıcı olması 
+
+// properties persistent mesajları kalıcı hale getirir. rabbitmq sunucusunda problem olsa dahi 
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+for (int i = 0; i < 10; i++)
+{
+    byte[] messages = Encoding.UTF8.GetBytes("Merhaba");
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: messages,basicProperties:properties);
+}
+
+Console.Read();
